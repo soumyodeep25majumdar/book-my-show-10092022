11dc8fb7819326e8cbb09f7966097fa846278c4d
soumyodeep25majumdar <soumyodeep.majumder123@gmail.com>
2022-09-13 02:04:06 +0530

    Completed banner and hero carousel for main page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 
 import './App.css';
+//Routing
+import { Routes, Route } from 'react-router-dom';
 
 //React slick css for carousel
-import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+//Pages
+import HomePage from './Pages/Home.page';
+import MoviePage from './Pages/Movie.page';
+import PlayPage from './Pages/Play.page';
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/movie/:id" element={<MoviePage />} />
+      <Route path="/plays" element={<PlayPage />} />
+    </Routes>
   );
 }
+
 
 export default App;
